dff2b4bad9c262fe651c13add19cdc8bcd6b2db2
JacobTWeeDzYTweed <31289925+JacobTWeeDzYTweed@users.noreply.github.com>
2018-02-14 13:12:58 -0700

    Add files via upload

---
+++ classPyramid.cs
@@ -0,0 +1,41 @@
+using System;
+class Pyramid{
+
+    public double length;
+    public double breadth;
+    public double height;
+
+    public Pyramid()
+    {
+        length = 0;
+        breadth = 0;
+        height = 0;
+    }
+
+    public void SetLength(double input)
+    {
+        length = input;
+    }
+
+    public void SetBreadth(double input)
+    {
+        breadth = input;
+    }
+
+    public void SetHeight(double input)
+    {
+        height = input;
+    }
+
+    public double GetVolume()
+    {
+        double volume = breadth*length*height*0.5;
+        return volume;
+    }
+
+    public double GetSurfaceArea()
+    {
+        double surfaceArea = length*breadth + length*height + breadth*height;
+        return surfaceArea;
+    }
+}
